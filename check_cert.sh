d82537b82d81c55eb876c4bcb10ead2fe6327dfd
root <root@bastion.qocp4.example.com>
2020-12-15 03:19:03 +0000

    update

--- check_cert.sh
+++ check_cert.sh
@@ -6,7 +6,11 @@ cd /tmp
 oc -n openshift-kube-apiserver-operator get cm kube-apiserver-to-kubelet-client-ca -o jsonpath='{.data.ca-bundle\.crt}' > ca-bundle.crt
 csplit -f kube-apiserver-to-kubelet-client-ca- ca-bundle.crt '/-----BEGIN CERTIFICATE-----/' '{*}' 2>&1 > /dev/null
 for cert in `ls kube-apiserver-to-kubelet-client-ca-{01..99} 2> /dev/null` ; do
-openssl x509 -dates -noout -in $cert | grep notAfter
+CERTDATE=$(openssl x509 -dates -noout -in $cert | grep notAfter)
+CERTDATE=${CERTDATE/notAfter=/}
+CERTDATE=$(TZ=Asia/Shanghai date --date="$CERTDATE")
+CERTROT=$(TZ=Asia/Shanghai date --date="$CERTDATE 73 days ago")
+echo "$CERTROT|$CERTDATE"
 done
 
 # CLEAN UP THE EXTRACTED CERTIFICATES
