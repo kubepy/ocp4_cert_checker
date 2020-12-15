b5f10fa822e09fb601685307c45bcd5070441513
root <root@bastion.qocp4.example.com>
2020-12-15 02:31:22 +0000

    update

---
+++ check_cert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# WARNING: NEED TO USE '/tmp' PATH FOR SPLITTING THE CERTIFICATES
+cd /tmp
+
+# GET THE CERTIFICATE BUNDLE AND PRINT THE DATE
+oc -n openshift-kube-apiserver-operator get cm kube-apiserver-to-kubelet-client-ca -o jsonpath='{.data.ca-bundle\.crt}' > ca-bundle.crt
+csplit -f kube-apiserver-to-kubelet-client-ca- ca-bundle.crt '/-----BEGIN CERTIFICATE-----/' '{*}' 2>&1 > /dev/null
+for cert in `ls kube-apiserver-to-kubelet-client-ca-{01..99} 2> /dev/null` ; do
+openssl x509 -dates -noout -in $cert | grep notAfter
+done
+
+# CLEAN UP THE EXTRACTED CERTIFICATES
+for cert in `ls kube-apiserver-to-kubelet-client-ca-{00..99} 2> /dev/null` ; do
+rm -rf $cert
+done
